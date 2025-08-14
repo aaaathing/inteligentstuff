3fbd997b8ca519772576813117ee552236989ec4
aaaathing <aarontao950@gmail.com>
2025-08-13 18:42:23 -0700

    Start adding outcome predictor

---
+++ goal-considerer.py
@@ -0,0 +1,86 @@
+import torch
+from torch import tensor
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+goalProposer = nn.Sequential(
+		nn.Linear(4, 128),
+		nn.ReLU(),
+		nn.Linear(128, 128),
+		nn.ReLU(),
+		nn.Linear(128, 2),
+		nn.Softmax()
+)
+rewardPredictor = nn.Sequential(
+		nn.Linear(4, 128),
+		nn.ReLU(),
+		nn.Linear(128, 128),
+		nn.ReLU(),
+		nn.Linear(128, 1)
+)
+
+class LaterPredictingLayer:
+	def __init__(self, shape: int):
+		self.shape = shape
+		self.w = {}
+		self.senderTrace = {}
+		self.nextV = torch.zeros(shape)
+	def input(self, sender):
+		if not sender in self.w:
+			self.w[sender] = torch.rand((sender.shape,self.shape))*0.2+0.4
+		self.nextV += sender.v @ self.w[sender]
+	def inputTensor(self, t):
+		self.nextV += t.flatten()
+	def update(self, reward=0.0, lr=0.5, achLearningSignal = 0.0):
+		for sender in self.w:
+			if not sender in self.senderTrace:
+				self.senderTrace[sender] = torch.zeros((sender.shape))
+			self.senderTrace[sender] += sender.v * achLearningSignal
+			self.w[sender] += achLearningSignal * self.senderTrace[sender][:,None] * (self.nextV-self.v)[None,:] * lr
+			self.senderTrace[sender] *= max(1.0-achLearningSignal, 0.0)
+		self.v = self.nextV
+		self.nextV = torch.zeros(self.shape)
+		return self.v
+
+outcomePredictor = LaterPredictingLayer(4)
+# todo: go and nogo in vbg (final decider)
+
+
+fig, axs = plt.subplots(1, 2)
+
+for i in range(10):
+		state = tensor([1.0, 0.0, 0.0, 0.0])
+		action_probs = goalProposer(state)
+		action = torch.multinomial(action_probs, 1)
+		
+		# Simulate a reward
+		with torch.no_grad():
+			reward = tensor([1.0]) if action.item() == 0 else tensor([-1.0])
+		
+		# Critic evaluation
+		value = rewardPredictor(state)
+		
+		# Loss calculation
+		advantage = reward - value
+		actor_loss = -torch.log(action_probs[action]) * advantage
+		critic_loss = advantage.pow(2)
+		
+		# Backpropagation
+		goalProposer.zero_grad()
+		rewardPredictor.zero_grad()
+		(actor_loss + critic_loss).backward()
+		
+		with torch.no_grad():
+			# Update weights
+			for param in goalProposer.parameters():
+					param.data -= 0.01 * param.grad.data
+			for param in rewardPredictor.parameters():
+					param.data -= 0.01 * param.grad.data
+
+		print(action_probs)
+		axs[0].imshow(action_probs.detach()[:,None], interpolation='nearest', vmin=0,vmax=1)
+		axs[1].clear()
+		axs[1].text(0.4,0.4,value.item())
+		plt.pause(1)
