36e938fbde53e305fce27f696117e171b01ce05f
aaaathing <aarontao950@gmail.com>
2025-09-28 22:21:24 -0700

    Update ai.py, many new layers (Sep 27 - Sep 28) and update interact_browser.js (Sep 28)

--- ai.py
+++ ai.py
@@ -2,89 +2,231 @@ import torch
 from torch import tensor
 import torch.nn as nn
 import torch.nn.functional as F
-import parts
 import matplotlib.pyplot as plt
-
 import absvit
 
-whereInputLayer = parts.Layer(28*28)
-whatInputLayer = parts.Layer(192)
 
-positiveOutcomePredictor = parts.OutcomePredictingLayer(4)
-negativeOutcomePredictor = parts.OutcomePredictingLayer(4)
+# variables that can be changed
+class vars:
+	lr = 0.5
+	hasReward = 0.0
+	alphaCycleProgress = {"minusPhaseEnd":False, "end":False, "plusPhase":False}
+	traceDecay = 0.1
 
-gatingWindowLayer = parts.GatingWindowLayer()
 
-yesLayer = parts.DecideLayer(4)
-noLayer = parts.DecideLayer(4)
-actionLayer = parts.DecideLayer(4)
+def initWeights(shape):
+	#match initWeights:
+	#	case "zero":
+	#		return torch.rand()*0.1
+	#	case "random":
+			return torch.rand(shape)*0.2+0.4
+		
+class Layer:
+	def __init__(self, shape: int):
+		self.shape = shape
+		self.inputExcitatory = torch.zeros(shape)
+		self.inputInhibition = torch.zeros(shape)
+		self.v = torch.zeros(shape)
+		self.output = torch.zeros(shape)
+		self.prevOutput = torch.zeros(shape)
+		self.w = {}
+		self.feedbackInhibition = torch.zeros(shape)
+		self.constantOutput = None
 
-fig, axs = plt.subplots(3, 3)
+	def input(self, sender, inhibit=False, bidirectional=True):
+		if not sender in self.w:
+			self.w[sender] = initWeights((sender.shape,self.shape))
+		if inhibit:
+			self.inputExcitatory -= sender.output @ self.w[sender]
+		else:
+			self.inputExcitatory += sender.output @ self.w[sender]
+			if bidirectional:
+				sender.inputExcitatory += (self.output @ self.w[sender].t())
 
-def updateLayers(alphaCycleProgress, whatwhere):
-	parts.alphaCycleProgress = alphaCycleProgress
+	def inputTensor(self, t):
+		self.inputExcitatory += t.flatten()
+	def updateInhibit(self):
+		feedforwardInhibition = max((self.inputExcitatory.mean()+self.inputExcitatory.max())/2.0 - 1.0, 0.0) # if input is more than 1, start inhibiting it
+		self.feedbackInhibition.lerp_(self.prevOutput.mean(), 0.5)
+		self.inputInhibition += feedforwardInhibition + self.feedbackInhibition
+
+	def updateV(self):
+		self.prevOutput = self.output
+		self.updateInhibit()
+		if self.constantOutput is not None:
+			self.output.copy_(self.constantOutput)
+		else:
+			netInput = self.inputExcitatory - self.inputInhibition
+			self.v += (netInput - self.v) * 0.5
+			self.output = torch.tanh(2.0*self.v.clamp_min(0.0))
+		self.inputExcitatory = torch.zeros(self.shape)
+		self.inputInhibition = torch.zeros(self.shape)
+	def update(self):
+		self.updateV()
+
+def phaseLearn(self):
+	if vars.alphaCycleProgress["minusPhaseEnd"]:
+		self.outputMinusPhase = self.output.clone()
+	if vars.alphaCycleProgress["end"]:
+		for sender in self.w:
+			self.w[sender] += (self.output-self.outputMinusPhase)[None,:] * sender.output[:,None] * 0.1
+			self.w[sender].clamp_(0.0,1.0)
+def traceRewardLearn(self):
+	if vars.alphaCycleProgress["end"]:
+		if not hasattr(self, "senderTrace"):
+			self.trace = {}
+		for sender in self.w:
+			if not sender in self.trace:
+				self.trace[sender] = torch.zeros((sender.shape,self.shape))
+			self.trace[sender] += (sender.output[:,None] * self.output[None,:] - self.trace[sender]) * vars.traceDecay
+			if vars.hasReward:
+				self.w[sender] += self.trace[sender] * vars.hasReward * vars.lr
+
+class DeeppredLayers:
+	" see 2025/09-09-temporal-predict "
+	def __init__(self, shape):
+		self.s = Layer(shape) # superficial
+		self.p = {} # predicting layers / pulvinar
+		self.c = Layer(shape) # previous superficial layer activation / CT
+		#self.cInput = Layer(shape)
+		self.lowerLayers = set()
+	def input(self, sender, *args, **kwargs):
+		if not sender in self.lowerLayers:
+			self.lowerLayers.add(sender)
+		self.s.input(sender, *args, **kwargs)
+		
+	def update(self):
+		self.c.update()
+		#self.c.input(self.cInput, bidirectional=False)
+		for lowerLayer in self.lowerLayers:
+			if not lowerLayer in self.p:
+				self.p[lowerLayer] = Layer(lowerLayer.shape)
+
+			if vars.alphaCycleProgress["plusPhase"]:
+				self.p[lowerLayer].constantOutput = lowerLayer.output
+			else:
+				self.p[lowerLayer].constantOutput = None
+				self.p[lowerLayer].input(self.c, bidirectional=False)
+
+			self.p[lowerLayer].update()
+			phaseLearn(self.p[lowerLayer])
+			self.s.input(self.p[lowerLayer], bidirectional=False)
+
+		self.s.update()
+		phaseLearn(self.s)
+		
+		if vars.alphaCycleProgress["end"]:
+			self.c.constantOutput = self.s.output.clone()
+
+class DecideLayer(Layer):
+	def update(self):
+		self.updateV()
+		phaseLearn(self)
+
+class MotorLayers:
+	def __init__(self, shape):
+		self.prevLayer = Layer(shape)
+		self.layer = Layer(shape)
+	def update(self, gate):
+		self.layer.input(self.prevLayer, bidirectional=False)
+		if vars.alphaCycleProgress["end"]:
+			self.prevLayer.constantOutput = self.layer.output.clone()
+		if vars.alphaCycleProgress["plusPhase"]:
+			self.layer.input(gate)
+		self.layer.update()
+		phaseLearn(self.layer)
+		traceRewardLearn(self.layer)
+
+class PFLayers(DeeppredLayers):
+	def __init__(self, shape):
+		super().__init__(shape)
+		self.mem = torch.zeros(shape)
+	def update(self, gate):
+		self.s.inputTensor(self.mem)
+		super().update()
+		if gate.output[0] > 0.5:
+			self.mem.copy_(self.s.output)
+
+
+whereInputLayer = Layer(28*28)
+whatInputLayer = Layer(192)
+
+yesLayer = DecideLayer(20)
+noLayer = DecideLayer(20)
+actionLayer_m1 = DecideLayer(20)
+actionLayer_wm1 = DecideLayer(1)
+
+wm1 = PFLayers(20)
+
+m1 = MotorLayers(15)
+
+def updateLayers(whatwhere):
 	learningSignal = 0.0
-	learningSignal += env.reward
+	learningSignal += abs(env.reward)
 
 	#if env.video[0,0,0].item()>0.5: # this is temporary, will be removed later
 	#	learningSignal = 1.0
-	
-	parts.lr = 0.5
 
 	whatInputLayer.inputTensor(whatwhere[0])
 	whereInputLayer.inputTensor(whatwhere[1])
 	whatInputLayer.update()
 	whereInputLayer.update()
-	
-	positiveOutcomePredictor.input(whatInputLayer)
-	positiveOutcomePredictor.input(whereInputLayer)
-	negativeOutcomePredictor.input(whatInputLayer)
-	negativeOutcomePredictor.input(whereInputLayer)
 
-	positiveOutcomePredictor.update(hasReward=env.reward, achLearningSignal=learningSignal)
-	negativeOutcomePredictor.update(hasReward=env.reward, achLearningSignal=learningSignal)
+	yesLayer.input(whatInputLayer)
+	yesLayer.input(whereInputLayer)
+	noLayer.input(whatInputLayer)
+	noLayer.input(whereInputLayer)
+	actionLayer_m1.input(yesLayer)
+	actionLayer_m1.input(noLayer, inhibit=True)
+	actionLayer_wm1.input(yesLayer)
+	actionLayer_wm1.input(noLayer, inhibit=True)
 
-	#print(positiveOutcomePredictor.prevV,positiveOutcomePredictor.senderTrace)
+	yesLayer.update()
+	noLayer.update()
+	actionLayer_m1.update()
+	actionLayer_wm1.update()
 
-	gatingWindowLayer.update([positiveOutcomePredictor,negativeOutcomePredictor])
-	gatingInhibit = max(1.0-gatingWindowLayer.totalReleased, 0.0)*10.0
+	wm1.input(whatInputLayer)
+	wm1.input(whereInputLayer)
+	wm1.update(actionLayer_wm1)
 
-	yesLayer.input(positiveOutcomePredictor)
-	yesLayer.inputInhibition += gatingInhibit
-	noLayer.input(negativeOutcomePredictor)
-	noLayer.inputInhibition += gatingInhibit
-	actionLayer.input(yesLayer)
-	actionLayer.inputInhibition += gatingInhibit
-	#actionLayer.input(noLayer, inhibit=True)
-
-	yesLayer.update(hasReward=env.reward)
-	noLayer.update(hasReward=env.reward)
-	actionLayer.update(hasReward=env.reward)
-
-	if alphaCycleProgress["end"]:
-		if actionLayer.output.max() > 0.5:
-			gatingWindowLayer.reset()
+	m1.layer.input(whatInputLayer)
+	m1.layer.input(whereInputLayer)
+	m1.update(actionLayer_m1)
 	
 	axs[1,0].clear()
-	axs[1,0].text(0.1,0.1, f"learningSignal: {learningSignal} \nreward: {env.reward} \ntotalReleased: {gatingWindowLayer.totalReleased}")
-	axs[0,0].imshow(env.video.reshape(224,224,4)); axs[0,0].set_title("video")
-	axs[0,1].imshow(whatInputLayer.output.view(12,16), vmin=0,vmax=1); axs[0,1].set_title("whatInputLayer")
-	axs[0,2].imshow(whereInputLayer.output.view(28,28), vmin=0,vmax=1); axs[0,2].set_title("whereInputLayer")
-	axs[2,0].imshow([positiveOutcomePredictor.output], vmin=0,vmax=1); axs[2,0].set_title("positiveOutcomePredictor")
-	axs[2,1].imshow([yesLayer.output], vmin=0,vmax=1); axs[2,1].set_title("yesLayer")
-	axs[2,2].imshow([actionLayer.output], vmin=0,vmax=1); axs[2,2].set_title("actionLayer")
+	axs[1,0].text(0.1,0.1, f"learningSignal: {learningSignal} \nreward: {env.reward}")
+	plotAt(0,0, env.video.reshape(224,224,4), "video")
+	plotAt(0,1, whatInputLayer.output.view(12,16), "whatInputLayer")
+	plotAt(0,2, whereInputLayer.output.view(28,28), "whereInputLayer")
+	#axs[2,0].imshow([positiveOutcomePredictor.output], vmin=0,vmax=1); axs[2,0].set_title("positiveOutcomePredictor")
+	plotAt(2,1, [yesLayer.output], "yesLayer")
+	plotAt(2,2, [actionLayer_m1.output], "actionLayer_m1")
+	plotAt(3,0, [m1.layer.output], "m1")
 	plt.pause(0.1)
+
+fig, axs = plt.subplots(4, 3)
+def plotAt(x,y, v, title):
+	axs[x,y].imshow(v, vmin=0,vmax=1)
+	axs[x,y].set_title(title)
+	
 
 
 async def ailoop():
 	for i in range(100):
-		await env.step(actionLayer.output)
+		await env.step(m1.layer.output)
+		vars.hasReward = env.reward
 
 		video = (tensor(env.video, dtype=torch.float) / 255.0).view(224,224,4)[:,:,0:3].permute(2,0,1)
 		whatwhere = absvit.run(video, whatInputLayer.output, whereInputLayer.output)
 
 		for j in range(10):
-			updateLayers({"end":j==9}, whatwhere)
+			vars.alphaCycleProgress = {"minusPhaseEnd": j==9, "end":False, "plusPhase":False}
+			updateLayers(whatwhere)
+
+		for j in range(10):
+			vars.alphaCycleProgress = {"minusPhaseEnd": False, "end":j==9, "plusPhase":False}
+			updateLayers(whatwhere)
 
 		plt.pause(5)
 
--- interact_browser.js
+++ interact_browser.js
@@ -10,23 +10,62 @@
 	video.autoplay=true
 	video.srcObject=stream
 	
-	let p = bot
-	let prevHealth = p.health, prevFood = p.food
+	let p
+	let prevHealth, prevFood
+	let prevMouseDown = [false, false]
 
 	let ws = new WebSocket("ws://localhost:8000");
 	ws.onmessage = function(event) {
-		let message = JSON.parse(event.data);
+		let msg = JSON.parse(event.data);
 		let reward = 0
-		if(prevHealth !== p.health){
-			if(p.health < prevHealth) reward -= prevHealth-p.health
-			prevHealth = p.health
+
+		if(p !== window.bot){
+			p = window.bot
+			// if bot has just been defined
+			if(p) prevHealth = p.health, prevFood = p.food
 		}
-		if(p.food<4){
-			reward -= (4-p.food)
+		// p and bot are the same
+		if(p){
+			if(msg.action[0] || msg.action[1]){
+				document.dispatchEvent(new MouseEvent('mousemove', {
+					movementX: (msg.action[0]-msg.action[1])*0.1,
+					movementY: (msg.action[2]-msg.action[3])*0.1,
+				}))
+			}
+
+			bot.setControlState('forward', msg.action[4]>0.5)
+			bot.setControlState('back', msg.action[5]>0.5)
+			bot.setControlState('left', msg.action[6]>0.5)
+			bot.setControlState('right', msg.action[7]>0.5)
+			bot.setControlState('jump', msg.action[8]>0.5)
+			bot.setControlState('sneak', msg.action[9]>0.5)
+			bot.setControlState('sprint', msg.action[10]>0.5)
+
+			let mouseDown = msg.action[11]>0.5
+			if(prevMouseDown[0] !== mouseDown){
+				prevMouseDown[0] = mouseDown
+				if(mouseDown[0]) document.dispatchEvent(new MouseEvent('mousedown', {button:0}))
+				else document.dispatchEvent(new MouseEvent('mouseup', {button:0}))
+			}
+			mouseDown = msg.action[12]>0.5
+			if(prevMouseDown[1] !== mouseDown){
+				prevMouseDown[1] = mouseDown
+				if(mouseDown[1]) document.dispatchEvent(new MouseEvent('mousedown', {button:2}))
+				else document.dispatchEvent(new MouseEvent('mouseup', {button:2}))
+			}
+
+			if(prevHealth !== p.health){
+				if(p.health < prevHealth) reward -= prevHealth-p.health
+				prevHealth = p.health
+			}
+			if(p.food<4){
+				reward -= (4-p.food)
+			}
+			if(prevFood !== p.food){
+				if(p.food > prevFood) reward += prevFood-p.food
+			}
 		}
-		if(prevFood !== p.food){
-			if(p.food > prevFood) reward += prevFood-p.food
-		}
+
 		ws.send(JSON.stringify({reward}));
 		let scale = Math.max(streamSettings.width/canvas.width, streamSettings.height/canvas.height)
 		ctx.drawImage(video, 0,0, streamSettings.width/scale, streamSettings.height/scale)
